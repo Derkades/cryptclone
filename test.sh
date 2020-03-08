a1d64412024ff2b336d8cc18a6e41a2bf60f9341
Derkades <robinslot@hotmail.nl>
2020-03-08 18:16:59 +0100

    Check restore content at end of test

--- test.sh
+++ test.sh
@@ -6,6 +6,7 @@ sudo rm -rf data-restore data-server data-sync
 
 sudo docker-compose rm -sf
 
+sudo docker-compose build
 sudo docker-compose up -d server
 sudo chown -R 33:33 data-server
 
@@ -15,3 +16,14 @@ echo "some test data" > data-sync/test.txt
 docker-compose run sync
 
 docker-compose run restore
+
+DATA=$(cat data-restore/documents/test.txt)
+
+if [ "$DATA" == "some test data" ]
+then
+    echo "Test succeeded"
+else
+    echo "Test failed"
+fi
+
+sudo rm -rf data-restore data-server data-sync
